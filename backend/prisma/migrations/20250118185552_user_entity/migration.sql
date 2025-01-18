46ac2713602a1c01c83c686f24ad37b89a90160c
Ari Rebouças <arireboucasdev@gmail.com>
2025-01-18 20:02:16 -0300

    feat: database

---
+++ backend/prisma/migrations/20250118185552_user_entity/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "Professor" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Classroom" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "subject" TEXT NOT NULL,
+    "professor_id" INTEGER NOT NULL,
+    CONSTRAINT "Classroom_professor_id_fkey" FOREIGN KEY ("professor_id") REFERENCES "Professor" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Activity" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "subject" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "classroom_id" INTEGER NOT NULL,
+    CONSTRAINT "Activity_classroom_id_fkey" FOREIGN KEY ("classroom_id") REFERENCES "Classroom" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
