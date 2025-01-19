59a1c06600d770ecc93ab4e4d08b9e12984b9fc7
Ari Rebouças <arireboucasdev@gmail.com>
2025-01-19 00:02:22 -0300

    feat: remove jest.config warning

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,18 +1,13 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    preset: 'ts-jest', // Usando ts-jest para permitir testes com TypeScript
-    testEnvironment: 'node', // Usando o ambiente de testes do Node.js
-    moduleFileExtensions: ['ts', 'js', 'json'], // Extensões de arquivos suportadas
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js', 'json'],
     transform: {
-        '^.+\.(t|j)sx?$': 'ts-jest', // Transforma arquivos .ts e .tsx com ts-jest
+        '^.+\\.(t|j)sx?$': ['ts-jest', { isolatedModules: true }],
     },
-    testMatch: ['<rootDir>/src/**/*.test.ts'], // Procura arquivos de teste que terminam com .test.ts na mesma pasta
-    globals: {
-        'ts-jest': {
-            isolatedModules: true, // Para melhorar a performance ao rodar testes
-        },
-    },
+    testMatch: ['<rootDir>/src/**/*.test.ts'],
 };
 
 export default config;
