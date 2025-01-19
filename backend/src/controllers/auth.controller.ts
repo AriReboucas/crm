8de314d82d8bdedac24f074199a2fd5238f4ca41
Ari Rebouças <arireboucasdev@gmail.com>
2025-01-18 21:58:14 -0300

    feat: add auth validations

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,23 +1,38 @@
 import { Request, Response } from "express";
 import { prisma } from "../server";
+import { loginSchema } from "../types/auth.types";
+import { ZodError } from "zod";
 
 export const login = async (req: Request, res: Response) => {
     try {
-        const { email, password } = req.body;
+        const body = loginSchema.parse(req.body);
+
+        let { email, password } = body;
+
+        email = email.toLowerCase();
 
         const loggedProfessor = await prisma.professor.findFirst({
             where: {
                 email,
-                password
-            }
-        })
+                password,
+            },
+        });
 
         if (!loggedProfessor) {
-            res.status(401).json({ error: "Invalid email or password" });
+            res.status(401).json({ error: "E-mail ou senha inválidos." });
         } else {
             res.status(200).json(loggedProfessor);
         }
     } catch (error) {
-        res.status(500).json({ error: error })
+        if (error instanceof ZodError) {
+            res.status(400).json({
+                errors: error.errors.map((err) => ({
+                    path: err.path,
+                    message: err.message,
+                })),
+            });
+        } else {
+            res.status(500).json({ error: "Erro interno do servidor." });
+        }
     }
-}
+};
